fd4d65d9c44ed8e5c72d58393650b7184f0cf2c2
naveenparamesh <3nt3rin123>
2016-02-20 16:30:53 -0500

    making clock.txt

--- workspace/ASCIIGameTemplate.java
+++ workspace/ASCIIGameTemplate.java
@@ -21,31 +21,31 @@ import java.util.zip.*;
 
 public class ASCIIGameTemplate {
 
-    static ASCIIScreen game;
+  static ASCIIScreen game;
 
-    public static void main(String[] args) throws IOException {
-        Character mask = null;
-        String trigger = null;
-	game = new ASCIIScreen();
-	
-        ConsoleReader reader = new ConsoleReader(System.in, new PrintWriter(System.out));
+  public static void main(String[] args) throws IOException {
+    Character mask = null;
+    String trigger = null;
+    game = new ASCIIScreen();
 
-	char[] allowed = {'i','j','k','l','q'};
+    ConsoleReader reader = new ConsoleReader(System.in, new PrintWriter(System.out));
 
-	game.init();
-	
-	try {
-	    int i = 0;
-	    while(i != (int)'q') {
-		i = reader.readCharacter(allowed);
-		System.out.println(i);
-		
-		game.processChar(i);
-		game.printScreen();
-		TimeUnit.MILLISECONDS.sleep(100);
-	    }
-	} catch (InterruptedException e) {
-	    e.printStackTrace();
-	}
+    char[] allowed = {'i','j','k','l','q'};
+
+    game.init();
+
+    try {
+      int i = 0;
+      while(i != (int)'q') {
+        i = reader.readCharacter(allowed);
+        System.out.println(i);
+
+        game.processChar(i);
+        game.printScreen();
+        TimeUnit.MILLISECONDS.sleep(100);
+      }
+    } catch (InterruptedException e) {
+      e.printStackTrace();
     }
+  }
 }
